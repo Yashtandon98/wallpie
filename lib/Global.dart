3897344d6741ac37e1d5bf4bff982a4c55a1c407
Yashtandon98 <40264382+Yashtandon98@users.noreply.github.com>
2020-02-19 01:33:26 +0530

    Getting wallpapers from pexels.

---
+++ lib/Global.dart
@@ -0,0 +1,7 @@
+import 'models/photos.dart';
+
+class Global{
+
+  static List<Photos> photos = new List();
+  static int index = 0;
+}
--- lib/wallpapers.dart
+++ lib/wallpapers.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'Global.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'models/photos.dart';
 
 class wallpapers extends StatefulWidget {
   @override
@@ -6,11 +10,33 @@ class wallpapers extends StatefulWidget {
 }
 
 class _wallpapersState extends State<wallpapers> {
+
+  getWallpapers() async {
+
+    await http.get("https://api.pexels.com/v1/curated?per_page=15&page=1", 
+    headers: {"Authorization":"563492ad6f91700001000001877263a8d7d7406f913242928a7c5f5e"}).then((res) {
+      print(res.body);
+      var parsedJson = jsonDecode(res.body);
+      Global.photos = (parsedJson["photos"] as List).map((data)=>Photos.fromJson(data)).toList();
+    });
+
+    setState(() {
+      
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getWallpapers(); 
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: GridView.builder(
+        itemCount: Global.photos.length,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
           mainAxisSpacing: 5, 
@@ -20,7 +46,10 @@ class _wallpapersState extends State<wallpapers> {
         itemBuilder: (context, index){
           return GestureDetector(onTap: () {
             Navigator.of(context).pushNamed('FullImage');
-          },child: Container(color: Colors.white));
+          },child: Container(decoration: BoxDecoration(
+            color: Colors.white,
+            image: DecorationImage(image: NetworkImage(Global.photos[index].src.tiny), fit: BoxFit.cover)
+          ),));
         }),
     );
   }
